8cc9f1f10542544e0241c77aa0bea14478db88cf
da0hn <gabrielhondacba@gmail.com>
2022-08-14 11:20:54 -0400

    feat: adiciona lista inicial de `Item`

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import * as C from './App.styles';
-
+import { useState } from 'react';
+import { Item } from './types/Item';
 
 const App = () => {
+
+  const [ list, setList ] = useState<Item[]>([
+    { id: 1, name: 'Comprar um pão na padaria', done: false },
+    { id: 2, name: 'Comprar um bolo na padaria', done: false },
+  ]);
+
   return (
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
+
+        {list.map((item, index) => (
+          <div>{item.name}</div>
+        ))}
+
+
       </C.Area>
     </C.Container>
   );
